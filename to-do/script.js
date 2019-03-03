1b37e9ae86849ce0a53bf16fcffc660402cb7d18
ka1myk <kalmyk.mp3@gmail.com>
2019-03-03 23:30:01 +0300

    03/03/19 23:30:01

--- to-do/script.js
+++ to-do/script.js
@@ -1,7 +1,39 @@
-$(function(){
-	$('#add-button').click(function(){
-		var newElement = $('<li class="item">');
-		newElement.text('');
-		$('ul').prepend(newElement);
-	});
+$(function(count){
+	var list = $('.item').length;
+	if (list != 0) 
+		{
+			$('#empty').css('display','none');
+		};
 });
+	
+$(document).ready(function(){
+	listenToAddButton();
+});
+function listenToAddButton(){
+$('#add-button').click(function(){
+		var newTaskTitle = $('#title').val();
+		var newTaskDiscrpt = $('#dscrpt').val();
+		var newItemTask = '<li class="item"><div class="item-title">'+ newTaskTitle +'<button class="item-clear"><img src="img/icon-clear.png" alt="del"></button><button class="item-hide"><img src="img/icon-arrow.png" alt="hide"></button></div><div class="item-dscrpt">'+ newTaskDiscrpt +'</div></li>';
+		$('ul').prepend(newItemTask);
+		$('#title').val('');
+		$('#dscrpt').val('');
+		$('#empty').css('display','none');
+		listenToDelButton();
+		/*listenToHideButton(); - пока не реализовал, но мысли были*/
+	});
+};
+
+function listenToDelButton(){
+$('.item-clear').click(function(){
+	$(this).parents('.item').remove();
+	$('#empty').css('display','block'); /*вообще это костыль, работает только если в списке был 1 элемент, см. ниже*/
+	/*$(function(check){
+		var list = $('.item').length;
+		if (list == 0) 
+			{
+				$('#empty').css('display','block'); - или не блок, моежет run-in лучше
+				(Устанавливает элемент как блочный или встроенный в зависимости от контекста.)
+			};
+	});	а вообще это не хочет работать, наверное выходной*/
+});
+};
